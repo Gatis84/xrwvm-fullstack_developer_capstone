3463e5077c11f43d8f327e69c053e1b29a8b86c6
i_code <gatis.paurans@gmail.com>
2025-08-21 08:18:34 -0400

    missed commits

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,14 +1,18 @@
 import Dealers from "./components/Dealers/Dealers";
-import LoginPanel from "./components/Login/Login"
+import Dealer from "./components/Dealers/Dealer";
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
+import PostReview from "./components/Dealers/PostReview";
 
 function App() {
   return (
     <Routes>
       <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
